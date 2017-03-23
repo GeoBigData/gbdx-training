86aa5bd18410fb628774e30b501dd9a077fd7917
lizasapphire <elizabeth.golden@colorado.edu>
2017-03-23 07:42:32 -0600

    Update run_clip_task_workflow.py

--- task_tutorial_files/run_clip_task_workflow.py
+++ task_tutorial_files/run_clip_task_workflow.py
@@ -3,19 +3,20 @@ gbdx = Interface()
 
 # build the source buckets for the image and the shapefile
 gbdx_info = gbdx.s3.info
-ntt_bucket = "s3://" + gbdx_info['bucket'] + "/" + gbdx_info['prefix']
-image_source_path = ntt_bucket + "/ntt_training_data"
-shp_source_path = ntt_bucket + "/ntt_training_data_shp"
+my_bucket = "s3://" + gbdx_info['bucket'] + "/" + gbdx_info['prefix']
+shp_path = my_bucket + "/demo_task_shp"
+
+image_path = gbdx.catalog.get_data_location(catalog_id='10400100245B7800')
 
 # create the target path
-target_path = "DL_output"
+target_path = "demo_task_output"
 
 # declare the clip task with input ports
-ntt_task = gbdx.Task("clip_raster_DL",input_image=image_source_path,input_shapefile=shp_source_path)
+clip_task = gbdx.Task("clip_raster_ESG", input_image=image_path, input_shapefile=shp_path)
 
 # define the workflow and output data location
-workflow = gbdx.Workflow([ntt_task])
-workflow.savedata(ntt_task.outputs.data_out,location=target_path)
+workflow = gbdx.Workflow([clip_task])
+workflow.savedata(clip_task.outputs.data_out,location=target_path)
 
 # execute the workflow and return the workflow id/status
 workflow.execute()
