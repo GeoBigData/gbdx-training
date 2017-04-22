3ef6cf9070e1e945fb8d77f7d554c9564c0259c6
lizasapphire <elizabeth.golden@colorado.edu>
2017-04-21 19:34:01 -0600

    Update clip_task_workflow.py

--- task_tutorial_files/clip_task_workflow.py
+++ task_tutorial_files/clip_task_workflow.py
@@ -9,7 +9,7 @@ source_s3 = gbdx.catalog.get_data_location(catalog_id='10400100245B7800')
 shape_path = 's3://tutorial-files/this_shp_will_clip_10400100245B7800/'
 
 # define the 'AOP_Strip_Processor' 
-aop_task = gbdx.Task('AOP_Strip_Processor', data=image_path, enable_pansharpen=True)
+aop_task = gbdx.Task('AOP_Strip_Processor', data=source_s3, enable_pansharpen=True)
 
 # define the 'gdal_cli' Task
 glue_task = gbdx.Task('gdal-cli', data=aop_task.outputs.data.value, execution_strategy='runonce',
